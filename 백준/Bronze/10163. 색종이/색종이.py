4d34055a49e1935f5ac8344f27f6413dd80124fd
heeyoung <122499570+hi9900@users.noreply.github.com>
2023-03-15 00:26:56 +0900

    [Bronze I] Title: 색종이, Time: 848 ms, Memory: 121620 KB -BaekjoonHub

---
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+data = [[0] * 1002 for _ in range(1002)]
+cnt_l = []
+paper = []
+for p in range(N):
+    sj, si, b, h = map(int, input().split())
+    paper.append((si, sj, b, h, p+1))
+while paper:
+    si, sj, b, h, p = paper.pop()
+    cnt = 0
+    for i in range(si, si+h):
+        for j in range(sj, sj+b):
+            if data[i][j] == 0:
+                data[i][j] = p
+                cnt += 1
+    cnt_l.append(cnt)
+
+print(*cnt_l[::-1], sep="\n")
