5836886b3fa13a5fecbd0b3fcf91440d0654e118
heeyoung <122499570+hi9900@users.noreply.github.com>
2023-03-22 00:20:17 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+x = []
+y = []
+for _ in range(3):
+    i, j = map(int, input().split())
+    if i in x:
+        x.remove(i)
+    else:
+        x.append(i)
+    if j in y:
+        y.remove(j)
+    else:
+        y.append(j)
+
+print(*x, *y)
