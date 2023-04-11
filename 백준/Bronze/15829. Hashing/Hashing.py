f4b9c08ed29ebc61d9880f2073e07ec305a25740
heeyoung <122499570+hi9900@users.noreply.github.com>
2023-04-12 01:42:23 +0900

    [Bronze II] Title: Hashing, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+L = int(input())
+string = input()
+answer = 0
+
+for i in range(L):
+    answer += (ord(string[i])-96) * (31 ** i)
+print(answer % 1234567891)
