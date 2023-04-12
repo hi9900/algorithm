d2b6e967589711b497df4e5778ec6c5aa45f592c
heeyoung <122499570+hi9900@users.noreply.github.com>
2023-04-12 22:06:08 +0900

    [Silver II] Title: 최소 힙, Time: 124 ms, Memory: 37180 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+N = int(input())
+data = []
+for _ in range(N):
+    x = int(input())
+
+    if x == 0:
+        if len(data) == 0:
+            print(0)
+        else:
+            # 배열에서 가장 작은 값을 출력하고, 그 값을 배열에서 제거
+            print(heapq.heappop(data))
+    else:
+        # 배열에 x를 추가
+        heapq.heappush(data, x)
