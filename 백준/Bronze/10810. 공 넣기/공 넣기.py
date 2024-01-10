3c20b6676a2965311bccf5259ad2610205d6e490
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-10 16:49:19 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,13 +1,28 @@
 import sys
 input = sys.stdin.readline
-n, m = map(int, input().split())
 
-data = [0] * n
+"""
+N개의 바구니
+1 ~ N 번의 번호 공
+첫 바구니는 비어있고, 바구니에는 공 1개만
 
-for _ in range(m):
-    s, e, b = map(int, input().split())
-    for i in range(s-1, e):
-        data[i] = b
-print(*data)
+M번 공을 넣을 거임
+공을 넣을 바구니 범위(연속)를 정하고
+같은 번호의 공을 바구니에 넣는다.
+    이미 공이 들어 있다면, 빼고 새 공을 넣는다.
 
+M번 공을 넣은 후에 바구니의 상태
+"""
 
+N, M = map(int, input().split())
+
+arr = [0] * (N + 1)
+
+for _ in range(M):
+    # i번 ~ j번 바구니에 k번 공을 넣는다.
+    i, j, k = map(int, input().split())
+
+    for x in range(i, j+1):
+        arr[x] = k
+
+print(*arr[1:])
