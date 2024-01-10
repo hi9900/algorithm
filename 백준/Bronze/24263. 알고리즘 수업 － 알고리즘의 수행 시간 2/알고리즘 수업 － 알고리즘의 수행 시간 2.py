e04ccc601c80137e8ffaeaa5730899e0baaa8507
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-10 15:27:40 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
@@ -1,6 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().rstrip())
-print(n)
+N = int(input())
+print(N)
 print(1)
+"""
+sum = 0
+A = [numbers]
+for i in range(1, n+1):
+    sum += A[i]
+
+리스트 A의 크기 N에 비례함
+O(N)
+"""
