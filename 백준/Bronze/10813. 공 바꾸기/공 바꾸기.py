23cd75c8c76ff5679aae8edc3668a9d512956ebd
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-10 16:52:00 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,12 +1,19 @@
 import sys
 input = sys.stdin.readline
-n, m = map(int, input().split())
 
-data = list(range(1, n+1))
+"""
+N개의 바구니
+공이 1개씩 들어있음, 바구니와 같은 번호공
 
-for _ in range(m):
+M번 공을 바꾼다.
+바구니 2개의 공을 서로 교환
+"""
+
+N, M = map(int, input().split())
+arr = list(range(N+1))
+
+for _ in range(M):
     i, j = map(int, input().split())
+    arr[i], arr[j] = arr[j], arr[i],
 
-    data[i-1], data[j-1] = data[j-1], data[i-1]
-
-print(*data)
+print(*arr[1:])
