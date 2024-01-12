89628121413dda32345fb319c3c6ff01ada5fd3c
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-12 15:48:34 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,10 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-string = input().rstrip()
+word = input().rstrip()
 
-if string == string[::-1]:
+if word == word[::-1]:
     print(1)
 else:
     print(0)
-
