3c20fdd33b12a9e33a526db4b3f36a0ae2694ba4
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-12 16:01:57 +0900

    [Bronze III] Title: 배수와 약수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.py
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -5,9 +5,10 @@ while 1:
     a, b = map(int, input().split())
     if a + b == 0:
         break
+
     if b % a == 0:
-        print("factor")
+        print('factor')
     elif a % b == 0:
-        print("multiple")
+        print('multiple')
     else:
-        print("neither")
+        print('neither')
