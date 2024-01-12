4a316d371f871abadf15f590033b58bd839bf216
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-12 16:07:28 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.py
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.py
@@ -2,8 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+star = '*'
 
-for i in range(N, 0, -1):
-    ans = " " * (N-i) + "*" * (2*i-1)
-    print(ans)
-
+for i in range(2*N-1, 0, -2):
+    print(f'{star*i:^{2*N-1}}'.rstrip())
