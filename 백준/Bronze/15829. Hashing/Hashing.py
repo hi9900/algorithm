688d09fd42dc9ad9b9a75d7bc51afb052ad7449b
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-13 16:20:30 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -2,9 +2,14 @@ import sys
 input = sys.stdin.readline
 
 L = int(input())
-string = input()
-answer = 0
+word = input().rstrip()
 
+r = 31
+M = 1234567891
+
+# 영문 소문자 -> 숫자 * r**i -> 합
+sum_nums = 0
 for i in range(L):
-    answer += (ord(string[i])-96) * (31 ** i)
-print(answer % 1234567891)
+    sum_nums += (ord(word[i]) - 96) * (r ** i)
+
+print(sum_nums % M)
