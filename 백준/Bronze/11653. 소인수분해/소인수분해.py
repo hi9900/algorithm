588dca1bb99d8878f642c9d4518af4b5a3ccfb62
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-15 14:12:04 +0900

    [Bronze I] Title: 소인수분해, Time: 1168 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,26 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def baek(N):
-    if N == 1:
-        return
-    for i in insu:
-        if N % i == 0:
-            print(i)
-            return baek(N//i)
-
 N = int(input())
+if N == 1:
+    print("")
 
-insu = []
-
-for i in range(2, N+1):
-    if N % i == 0:
-        for j in range(2, i):
-            if i % j == 0:
-                break
+# 소인수 분해
+# N보다 작은 소수로 나누기
+else:
+    for i in range(2, N+1):
+        if N == 1:
+            break
         else:
-            insu.append(i)
-insu.append(N)
-
-baek(N)
-
+            # i는 소수이므로, N에서 최대한 나누기
+            # 소수가 아닌 수는 이미 나눠 졌으므로, 소수를 판별 할 필요가 없음
+            while 1:
+                if N % i == 0:
+                    N //= i
+                    print(i)
+                else:
+                    break
