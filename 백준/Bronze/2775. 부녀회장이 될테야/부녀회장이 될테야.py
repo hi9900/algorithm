7715a6f0ada09485516b5ecb3e92943f3c3ba62a
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-15 13:48:06 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,14 +1,16 @@
 import sys
 input = sys.stdin.readline
 
+# k층 n호 = k-1층 n호 + k층 n-1호
+build = [list(range(15))] + [[0, 1] + [0] * 13 for _ in range(14)]
+
+for i in range(1, 15):
+    for j in range(1, 15):
+        build[i][j] = build[i-1][j] + build[i][j-1]
+
 T = int(input())
-
 for _ in range(T):
-    K = int(input())
-    N = int(input())
+    k = int(input())
+    n = int(input())
 
-    data = [x for x in range(N+1)]
-    for i in range(K):
-        for j in range(1, N+1):
-            data[j] = data[j-1] + data[j]
-    print(data[-1])
+    print(build[k][n])
