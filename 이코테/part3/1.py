da742b9d4b7ae7701c6cef68b7e85eace03a1649
구희영 <9hiyoung@gmail.com>
2024-01-15 18:52:09 +0900

    Update: 1 해설 및 풀이 수정

--- 이코테/part3/1.py
+++ 이코테/part3/1.py
@@ -7,15 +7,13 @@ arr.sort()
 result = 0
 # 한 그룹에 속한 인원
 cnt = 0
-# 한 그룹의 최대 공포도
-max_x = 0
+
 for i in range(N):
-    max_x = max(arr[i], max_x)
     cnt += 1
 
     # 사람수 >= 공포도인 순간, 그룹 결성
-    if cnt >= max_x:
+    if cnt >= arr[i]:
         result += 1
-        cnt, max_x = 0, 0
+        cnt = 0
 
 print(result)
