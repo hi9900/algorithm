209749146f37d365539e79f87cb27e9910f39b7f
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-16 15:23:30 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.py
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.py
@@ -1,11 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-
-for i in range(N):
-    ans = "*" * (i+1)
-    print(ans)
-for i in range(N):
-    ans = "*" * (N-i-1)
-    print(ans)
+n = int(input())
+star = '*'
+for i in range(1, 2*n):
+    if i > n:
+        i = n - abs(i-n)
+    print(i*star)
