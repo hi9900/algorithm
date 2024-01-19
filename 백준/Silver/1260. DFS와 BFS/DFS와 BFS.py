e5d99a6c6375e9371b5f6fcf09cdd4af1db59cc3
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-19 14:41:05 +0900

    [Silver II] Title: DFS와 BFS, Time: 76 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,46 +1,40 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
-from collections import deque
-
-stack = []
-v = set()
-def DFS(start):
-    stack.append(start)
-    v.add(start)
-
-    while stack:
-        c = stack.pop()
-        print(c, end=" ")
-        for i in lst[c]:
-            if i not in v:
-                DFS(i)
 
 
-def BFS(start):
-    queue = deque()
-    queue.append(start)
-    v_b = set()
-    v_b.add(start)
-    while queue:
-        c = queue.popleft()
-        print(c, end=" ")
-        for i in lst[c]:
-            if i not in v_b:
-                queue.append(i)
-                v_b.add(i)
+def DFS(start, data, visited):
+    visited[start] += 1
+    print(start, end=" ")
+    for i in data[start]:
+        if visited[i] == 0:
+            DFS(i, data, visited)
 
 
-N, M, V = map(int,input().split())
-lst = [[] for _ in range(N+1)]
+def BFS(start, data, visited):
+    q = deque()
+    q.append(start)
+    visited[start] = 1
+    while q:
+        now = q.popleft()
+        print(now, end=" ")
+        for i in data[now]:
+            if visited[i] == 0:
+                visited[i] = 1
+                q.append(i)
 
+
+N, M, V = map(int, input().split())
+data = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+visited1 = [0] * (N+1)
 for _ in range(M):
-    v1, v2 = map(int, input().split())
-    lst[v1].append(v2)
-    lst[v2].append(v1)
-    lst[v1].sort()
-    lst[v2].sort()
+    a, b = map(int, input().split())
+    data[a].append(b)
+    data[b].append(a)
+for i in range(1, N+1):
+    data[i].sort()
 
-# print(lst)
-DFS(V)
+DFS(V, data, visited)
 print()
-BFS(V)
+BFS(V, data, visited1)
