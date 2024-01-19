edaae6bae8b6adf716f4f325b1e9a5e96b7d2a00
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-19 16:17:31 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,39 @@
+def solution(s):
+    N = len(s)
+
+    min_len = N
+    cnt = 1
+    for i in range(N // 2, 0, -1):
+        c_word = ''
+
+        # 맨 앞 자른거
+        key = s[0:i]
+        # print(key, end=" -> ")
+
+        # 단위 별로 자르기
+        for j in range(i, N, i):
+            if len(c_word) >= min_len:
+                break
+            # 압축
+            if key == s[j:j + i]:
+                cnt += 1
+
+            # 다른게 나오면, 이전 값 압축 표현 후 갱신
+            else:
+                if cnt == 1:
+                    c_word += key
+                else:
+                    c_word += f'{cnt}{key}'
+                key = s[j:j + i]
+                cnt = 1
+
+        # 마지막 잘린 거 더하기
+        if cnt == 1:
+            c_word += key
+        else:
+            c_word += f'{cnt}{key}'
+
+        # print(c_word)
+        min_len = min(min_len, len(c_word))
+    return min_len
+
