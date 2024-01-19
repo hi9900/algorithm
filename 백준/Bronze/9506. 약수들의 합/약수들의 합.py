a8f4eca7d853c9d45d1db6acafa5edd4bd25cd43
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-19 15:17:47 +0900

    [Bronze I] Title: 약수들의 합, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.py
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -1,18 +1,22 @@
 import sys
 input = sys.stdin.readline
 
+# 자신을 제외한 모든 약수들의 합
 while 1:
-    N = int(input())
-    if N == -1:
+    n = int(input())
+    if n == -1:
         break
-    data = []
-    for i in range(1, N):
-        if N % i == 0:
-            data.append(i)
 
-    if sum(data) == N:
-        print(f"{N} = ", end="")
-        print(*data, sep=" + ")
+    lst = []
+    sum_n = 0
+    for i in range(1, n):
+        if n % i == 0:
+            sum_n += i
+            lst.append(i)
 
+    if sum_n == n:
+        print(n, '=', end=" ")
+        print(*lst, sep=" + ")
     else:
-        print(f"{N} is NOT perfect.")
+        print(f'{n} is NOT perfect.')
+        
