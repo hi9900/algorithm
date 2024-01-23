db23e8fffee046aecc2f95c26143893154df0647
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-23 20:01:14 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34220 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,28 +1,26 @@
+from collections import deque
 import sys
-# input = sys.stdin.readline
-
-
-def bfs():
-    q = []
-    v = set()
-
-    q.append((0, 0))
-    v.add((0, 0))
-
-    while q:
-        ci, cj = q.pop(0)
-        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1),):
-            ni, nj = ci+di, cj+dj
-            if 0 <= ni < N and 0 <= nj < M:
-                if (ni, nj) not in v and data[ni][nj] == 1:
-                    data[ni][nj] += data[ci][cj]
-                    q.append((ni, nj))
-                    v.add((ni, nj))
-    return
-
+input = sys.stdin.readline
 
 N, M = map(int, input().split())
-data = [list(map(int, input())) for _ in range(N)]
 
-bfs()
-print(data[N-1][M-1])
+maze = [['0']*(M+2)] + [['0'] + list(input().rstrip()) + ['0'] for _ in range(N)] + [['0']*(M+2)]
+v = [[0] * (M+2) for _ in range(N+2)]
+
+dxy = ((-1, 0), (1, 0), (0, -1), (0, 1),)
+q = deque()
+# start: 1, 1 // end: N, M
+# x, y, cnt
+q.append((1, 1, 1))
+v[1][1] = 1
+while q:
+    x, y, c = q.popleft()
+    if x == N and y == M:
+        print(c)
+        break
+
+    for dx, dy in dxy:
+        nx, ny = x+dx, y+dy
+        if maze[nx][ny] == '1' and v[nx][ny] == 0:
+            q.append((nx, ny, c+1))
+            v[nx][ny] = 1
