4798023d1961bda522c0b0ba145f5dd9cfde83f8
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-23 19:47:59 +0900

    [Silver III] Title: 소수 구하기, Time: 5276 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -3,15 +3,11 @@ input = sys.stdin.readline
 
 M, N = map(int, input().split())
 
-# [0, 1 = False] + [2 ~ N]
-a = [False, False] + [True] * (N-1)
-
-# 1부터 N까지
-for i in range(N+1):
-    if a[i]:
-        for j in range(2*i, N+1, i):
-            a[j] = False
-
 for i in range(M, N+1):
-    if a[i]:
+    if i == 1:
+        continue
+    for j in range(2, int(i**0.5) + 1):
+        if i % j == 0:
+            break
+    else:
         print(i)
