877dcbd76dfc54e4a366a5af6de3dcb9c02f64a7
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-25 17:18:08 +0900

    [Silver I] Title: 숨바꼭질, Time: 1048 ms, Memory: 72460 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -2,29 +2,28 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-
-def move(X, c):
-    q = deque()
-    v = [0] * 100001
-    q.append((X, c))
-    v[X] += 1
-
-    if X == K:
-        return 0
-
-    while q:
-        c = q.popleft()
-        if c[0] == K:
-            return c[1]
-        for d in (1, -1, c[0]):
-            m = c[0] + d
-            if m < 0 or m > 100000:
-                continue
-            if not v[m]:
-                q.append((m, c[1]+1))
-                v[m] += 1
-
-
 N, K = map(int, input().split())
-print(move(N, 0))
 
+check = [0] * (100_001)
+
+q = deque()
+q.append((N, 0))
+min_time = 100_001
+
+while q:
+    now, time = q.popleft()
+    check[now] = time
+
+    if now == K:
+        print(time)
+        break
+
+    else:
+        if 0 <= now+1 <= 100_000 and check[now+1] == 0:
+            q.append((now+1, time+1))
+
+        if 0 <= now-1 <= 100_000 and check[now-1] == 0:
+            q.append((now-1, time+1))
+
+        if 0 <= now*2 <= 100_000 and check[now * 2] == 0:
+            q.append((now*2, time+1))
