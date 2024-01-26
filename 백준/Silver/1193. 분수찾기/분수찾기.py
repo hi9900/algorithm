a49319f0186c95de3b864917dd30b6e2cd21d740
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-01-26 17:51:02 +0900

    [Silver V] Title: 분수찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -3,25 +3,25 @@ input = sys.stdin.readline
 
 X = int(input())
 
-# 분자, 분모
-x, y = 1, 1
-cnt = 1
-
+line = 0
+end = 0
 while 1:
-    if cnt == X:
-        break
-    cnt += 1
-    if (x+y) % 2 == 1:
-        if y == 1:
-            x += 1
-        else:
-            x += 1
-            y -= 1
-    else:
-        if x == 1:
-            y += 1
-        else:
-            x -= 1
-            y += 1
+    line += 1
+    end += line
 
-print(f"{x}/{y}")
+    if X <= end:
+        break
+
+start = end - line + 1
+diff = X - start
+
+# 위 대각선
+if line % 2:
+    # 시작 좌표 = line, 1
+    target = (line-diff, 1+diff)
+# 아래 대각선
+else:
+    # 시작 좌표 = 1, line
+    target = (1+diff, line-diff)
+
+print(*target, sep="/")
