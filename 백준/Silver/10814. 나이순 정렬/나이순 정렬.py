55dfcfe4a3c8e47924e677013ac77304e080ac05
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-02-28 18:58:32 +0900

    [Silver V] Title: 나이순 정렬, Time: 4080 ms, Memory: 61632 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,13 +1,10 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
 lst = []
 for _ in range(N):
-    (age, name) = input().split()
-    lst.append((_, int(age), name))
+    a, b = input().split()
+    lst.append((_, a, b))
 
-lst = sorted(lst, key=lambda x: (x[1], x[0]))
+lst.sort(key=lambda x: (int(x[1]), x[0]))
 
-for _ in range(N):
-    print(*lst[_][1:])
+for i, *j in lst:
+    print(*j)
