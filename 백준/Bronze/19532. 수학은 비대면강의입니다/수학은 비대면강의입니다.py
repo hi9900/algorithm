84da6e59b33ecaf3a984dfeaceb85b09507148c6
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-02-28 18:46:48 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 696 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,13 +1,8 @@
-import sys
-input = sys.stdin.readline
 
 a, b, c, d, e, f = map(int, input().split())
-if (a == b == c == 0 and f == 0) or (d == e == f == 0 and c == 0):
-    x = 0
-    y = 0
 
-else:
-    x = (c * e - b * f) // (a * e - b * d)
-    y = (c * d - a * f) // (b * d - a * e)
-
-print(int(x), int(y))
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a*x + b*y == c and d*x + e*y == f:
+            print(x, y)
+            break
