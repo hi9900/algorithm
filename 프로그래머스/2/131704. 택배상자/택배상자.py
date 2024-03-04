ba185b9812c3a56e8f7aacc89fc1daf55ed22c46
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-03-05 00:09:33 +0900

    [level 2] Title: 택배상자, Time: 431.12 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+def solution(order):
+    N = len(order)
+    # q = deque(range(1, N+1))
+    # 보조 컨테이너 벨트는 선입후출 > stack
+    stack = []
+    
+    result = 0
+    main = 1
+    for i in range(N):
+        # 1. 꺼낼 수 있는 보조를 확인한다.
+        if stack and order[i] == stack[-1]:
+            stack.pop()
+            result += 1
+            continue
+            
+        # 2. 남은 메인을 확인한다.
+        # 2-1. 남은 메인에는 실어야 할 상자가 없음
+        if main > order[i]:
+            return result
+        # 2-2. 메인에는 실어야 할 상자가 있고, 
+        # 그 상자가 나올 때까지 보조로 이동
+        # 현재 메인 ~ order[i]-1 까지의 상자가 보조로 이동한다.
+        # range(q[0], order[i])
+        else:
+            stack += list(range(main, order[i]))
+            main = order[i] + 1
+            result += 1
+    return result
