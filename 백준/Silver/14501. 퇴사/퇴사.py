3f5131a054ab4f58f4753cfd0bfa23d1c102a23b
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-05-06 20:13:34 +0900

    [Silver III] Title: 퇴사, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,29 +1,21 @@
-import sys
-input = sys.stdin.readline
+def dfs(n, result):
+    global ans
 
+    if n >= N:
+        ans = max(ans, result)
+        return
 
-def sol2(day, remain, money):
-    global result
-    if day == N:
-        if remain > 0:
-            return 0
-        return money
-    result = sol2(day+1, remain-1, money)
-    if remain <= 0:
-        result = max(result, sol2(day+1, days[day]-1, money+pays[day]))
-    return result
+    if n+data[n][0] <= N:
+        dfs(n+data[n][0], result+data[n][1])
+    dfs(n+1, result)
 
 
 N = int(input())
-days = [0] * N
-pays = [0] * N
+data = []
 for _ in range(N):
     T, P = map(int, input().split())
-    days[_] = T
-    pays[_] = P
+    data.append((T, P))
 
-result = 0
-sol2(0, 0, 0)
-print(result)
-
-
+ans = 0
+dfs(0, 0)
+print(ans)
