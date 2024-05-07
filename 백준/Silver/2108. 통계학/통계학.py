f908bdd0aea43d7a7ce79dd0196d9888c54880b3
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-05-08 01:58:09 +0900

    [Silver III] Title: 통계학, Time: 536 ms, Memory: 58828 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,34 +1,33 @@
+import math
 import sys
-from collections import defaultdict
 input = sys.stdin.readline
 
-N = int(input())    # 홀수
+N = int(input())
 nums = []
-count_dict = defaultdict(int)
-
+dict_nums = dict()
 for _ in range(N):
-    num = int(input())  # 정수
+    num = int(input())
+
     nums.append(num)
-    count_dict[num] += 1
-nums.sort()
 
-# 산술평균
-avg = sum(nums) / N
-print(round(avg))
+    if num in dict_nums:
+        dict_nums[num] += 1
+    else:
+        dict_nums[num] = 1
 
-# 중앙값
-mid = N // 2
-print(nums[mid])
+# 1. 산술평균
+print(math.floor(sum(nums) / N + 0.5))
 
-# 최빈값
-count_lst = sorted(list(zip(count_dict.keys(), count_dict.values())), key=lambda x: (-x[1], x[0]))
-# print(count_lst)
-if len(count_lst) == 1:
-    print(count_lst[0][0])
-elif count_lst[0][1] == count_lst[1][1]:
-    print(count_lst[1][0])
+# 2. 중앙값
+sort_nums = sorted(nums)
+print(sort_nums[N // 2])
+
+# 3. 최빈값
+dict_nums_list = sorted(list(zip(dict_nums.keys(), dict_nums.values())), key=lambda x: (-x[1], x[0]))
+if len(dict_nums_list) >= 2 and dict_nums_list[0][1] == dict_nums_list[1][1]:
+    print(dict_nums_list[1][0])
 else:
-    print(count_lst[0][0])
+    print(dict_nums_list[0][0])
 
-# 최대최소 차이
-print(max(nums)-min(nums))
+# 4. 범위
+print(max(nums) - min(nums))
