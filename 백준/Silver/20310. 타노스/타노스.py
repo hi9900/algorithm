2964953bac97406d0ad4ddafc75fc7027b4d722a
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-05-12 18:58:09 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,7 +1,25 @@
 S = input()
-# 0의 개수 / 1의 개수 -> 절반
-# cnt_zero, cnt_one = S.count('0'), S.count('1')
+# 지워야 할 0, 1 개수
 cnt_zero, cnt_one = S.count('0') // 2, S.count('1') // 2
+# 앞에서부터 1 지우기 + 뒤에서부터 0 지우기
+cnt = 0
+while 1:
+    if cnt == cnt_one:
+        break
+    for i in range(len(S)):
+        if S[i] == '1':
+            cnt += 1
+            S = S[:i] + S[i+1:]
+            break
 
-# 사전순 -> 0 -> 1
-print('0' * cnt_zero + '1' * cnt_one)
+cnt = 0
+while 1:
+    if cnt == cnt_zero:
+        break
+    for i in range(len(S)-1, -1, -1):
+        if S[i] == '0':
+            cnt += 1
+            S = S[:i] + S[i + 1:]
+            break
+
+print(S)
