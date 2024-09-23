28e86e295cc43bfaa37ce38a677f5ee56e01780e
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-09-23 23:43:24 +0900

    [level 2] Title: 프로세스, Time: 0.66 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,20 +1,26 @@
 from collections import deque
 
 def solution(priorities, location):
-    answer = 0
-    q = deque()
-    for i in range(len(priorities)):
-        q.append((priorities[i], i))
+    q = list(enumerate(priorities))
+    count = 0
     
-    cnt = 0
     while q:
-        # q 안에 든 가장 높은 우선순위
-        max_q = max(q)[0]
+        count += 1
+        N = len(q)
+        run_i, run_p, idx = *q[0], 0
         
-        if q[0][0] < max_q:
-            q.rotate(-1)
-        else:
-            p, i = q.popleft()
-            cnt += 1
-            if i == location:
-                return cnt
+        for j in range(N):
+            i, p = q[j]
+            # 우선순위가 더 높은 프로세스 저장
+            if run_p < p:
+                idx = j
+                run_i, run_p = i, p
+                
+        # 프로세스 실행 (큐에서 삭제)
+        q = q[idx+1:] + q[:idx]
+        
+        # 찾는 프로세스이면, 값 반환
+        if run_i == location:
+            return count
+
+    return count
