27e378b34ad7676d2d94f8da1d52e9b14a8b17f0
heeyoung <122499570+hi9900@users.noreply.github.com>
2024-10-09 21:26:11 +0900

    [level 2] Title: 주식가격, Time: 187.88 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,13 +1,13 @@
 def solution(prices):
-    answer = [0] * len(prices)
-    stack = []
-    for i in range(len(prices)):
-        for s, index in stack:
-            answer[index] += 1
-        
-        while stack and stack[-1][0] > prices[i]:
-            stack.pop()
-        
-        stack.append((prices[i], i))
-        
+    N = len(prices)
+    answer = [0] * N
+    
+    for i in range(N):
+        for j in range(i+1, N):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+
     return answer
