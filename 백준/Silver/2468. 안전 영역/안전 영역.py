a03ca65ecf33d17dfaff17fc6091295f8b5e71c1
heeyoung <122499570+hi9900@users.noreply.github.com>
2025-08-07 17:34:41 +0900

    [Silver I] Title: 안전 영역, Time: 616 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,34 +1,45 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-sys.setrecursionlimit(10**7)
 
-xij = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-def dfs(v, i, j, N, h):
-    v[i][j] = 1
-    for (xi, xj) in xij:
-        ni, nj = i + xi, j + xj
-
-        if 0 <= ni < N and 0 <= nj < N:
-            if arr[ni][nj] > h and v[ni][nj] == 0:
-                dfs(v, ni, nj, N, h)
+dij = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
 
-N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-ans = 1
+def bfs(v, si, sj, n, h, arr):
+    q = deque()
+    q.append((si, sj))
+    v[si][sj] = 1
+
+    while q:
+        ii, jj = q.popleft()
+        for di, dj in dij:
+            ni, nj = ii + di, jj + dj
+            if 0 <= ni < n and 0 <= nj < n:
+                if v[ni][nj] == 0 and arr[ni][nj] > h:
+                    v[ni][nj] = 1
+                    q.append((ni, nj))
+    return
+
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+
+# 잠기는 높이 h = 1 -> max_h-1
 max_h = 0
-for i in range(N):
-    max_h = max(max_h, max(arr[i]))
+for i in range(n):
+    max_h = max(max(arr[i]), max_h)
 
+# 안전영역 safe
+safe = 1
 for h in range(1, max_h):
-    v = [[0] * N for _ in range(N)]
+    v = [[0] * n for _ in range(n)]
     cnt = 0
-
-    for i in range(N):
-        for j in range(N):
+    for i in range(n):
+        for j in range(n):
             if arr[i][j] > h and v[i][j] == 0:
                 cnt += 1
-                dfs(v, i, j, N, h)
-    ans = max(ans, cnt)
+                bfs(v, i, j, n, h, arr)
+        safe = max(safe, cnt)
 
-print(ans)
+print(safe)
