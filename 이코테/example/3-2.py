1c790259a171377f5612a5f7fc355b3dc89d23f1
heeyoung <9hiyoung@gmail.com>
2025-08-12 17:38:37 +0900

    250811: 이코테 복습 - 구현

--- 이코테/example/3-2.py
+++ 이코테/example/3-2.py
@@ -1,13 +1,20 @@
 N, M = map(int, input().split())
 A, B, d = map(int, input().split())
-
 data = [list(map(int, input().split())) for _ in range(N)]
-# N, M = 4, 4
-# A, B, d = 1, 1, 0
-# data = [
-#     [1, 1, 1, 1], [1, 0, 0, 1],
-#     [1, 1, 0, 1], [1, 1, 1, 1]
-# ]
+'''
+N, M = 4, 4
+A, B, d = 1, 1, 0
+data = [
+    [1, 1, 1, 1], [1, 0, 0, 1],
+    [1, 1, 0, 1], [1, 1, 1, 1]
+]
+'''
+
+# 방향: 0-북, 1-동, 2-남, 3-서
+# 왼쪽 이동 방향: 좌, 하, 우, 상
+dxy = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+# 후진 방향
+back = [(1, 0), (0, -1), (-1, 0), (0, 1)]
 
 visited = list([0] * M for _ in range(N))
 visited[A][B] = 1
@@ -16,12 +23,9 @@ cnt = 1
 while 1:
     # 네 방향
     for i in range(4):
-        #  왼쪽 방향: 북 동 남 서 -> 왼, 아래, 오, 위
-        fst = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-        dx, dy = fst[d]
+        dx, dy = dxy[d]
         nx, ny = A + dx, B + dy
 
-        # nx, ny는 맵 안
         if nx < 0 or nx >= N or ny < 0 or ny >= M:
             continue
 
@@ -36,8 +40,6 @@ while 1:
             d = (d+1) % 4
 
     else:
-        # 뒤로 한 칸: 북, 동, 남, 서 -> 아래, 왼, 위, 오
-        back = [(-1, 0), (0, -1), (1, 0), (0, +1)]
         bx, by = back[d]
         nx, ny = A + bx, B + by
 
