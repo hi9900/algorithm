62abdff3b21bae4405569db9eada96ce624bf6e0
heeyoung <9hiyoung@gmail.com>
2025-08-26 20:06:37 +0900

    250826: 이코테 복습 - 다이나믹프로그래밍

--- 이코테/example/7-1.py
+++ 이코테/example/7-1.py
@@ -14,3 +14,18 @@ for i in range(2, X+1):
         d[i] = min(d[i], d[i//5] + 1)
 
 print(d[X])
+
+"""
+# 풀이 2
+"""
+arr = [0] * (X + 1)
+arr[1] = 0
+INF = 10e7
+
+for i in range(2, X + 1):
+    arr[i] = 1 + min((arr[i//5] if i % 5 == 0 else INF),
+                     (arr[i//3] if i % 3 == 0 else INF),
+                     (arr[i//2] if i % 2 == 0 else INF),
+                     arr[i - 1])
+
+print(arr[X])
