a8cb869c7cd0493bb5e8f41f5f79951077770b48
heeyoung <9hiyoung@gmail.com>
2025-09-03 22:02:50 +0900

    250903: 이코테 복습 - dp

--- 이코테/example/7-3.py
+++ 이코테/example/7-3.py
@@ -5,7 +5,7 @@ d = [0] * 1001
 d[1] = 1
 d[2] = 3
 
-for i in range(3, N+1):
-    d[i] = d[i-2] * 2 + d[i-1]
+for i in range(3, N + 1):
+    d[i] = (d[i-1] + d[i-2] * 2) % 796_796
 
-print(d[N] % 796796)
+print(d[N])
