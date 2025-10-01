bf4b141d8d89fb613383f1df4365c49d6971bc15
heeyoung <122499570+hi9900@users.noreply.github.com>
2025-10-01 18:26:19 +0900

    [level 1] Title: 폰켓몬, Time: 0.70 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,4 @@
-from collections import Counter
-
 def solution(nums):
     N = len(nums)
-    
-    count = Counter(nums)
-    count_keys = len(count.keys())
-    
-    return min(N/2, count_keys)
+    count = len(set(nums))
+    return min(N/2, count)
