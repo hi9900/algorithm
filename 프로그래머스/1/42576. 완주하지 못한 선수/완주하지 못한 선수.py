fd19fd0e471e514ca996be3c3c4ecdfb77a2bc81
heeyoung <122499570+hi9900@users.noreply.github.com>
2025-10-01 18:11:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.69 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,9 @@
+from collections import Counter
+
 def solution(participant, completion):
-    participant_map = dict()
-    for p in participant:
-        if p in participant_map:
-            participant_map[p] += 1
-        else:
-            participant_map[p] = 1
-            
-    for c in completion:
-        participant_map[c] -= 1
-        
-    for name in participant_map:
-        if participant_map[name] == 1:
-            return name
+    part_count = Counter(participant)
+    comp_count = Counter(completion)
+    
+    diff = part_count - comp_count
+    
+    return list(diff.keys())[0]
