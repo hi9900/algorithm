1c9763016341658efa1fdf57b0511d2e54497592
heeyoung <122499570+hi9900@users.noreply.github.com>
2025-10-30 21:43:29 +0900

    [Bronze V] Title: 구구단, Time: 88 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+
+for (let i = 1; i <= 9; i++) {
+  console.log(`${n} * ${i} = ${n * i}`);
+}
