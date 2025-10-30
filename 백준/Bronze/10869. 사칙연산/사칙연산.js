940025b347f1a8f0067d48aa91439fe3b4cd4b50
heeyoung <122499570+hi9900@users.noreply.github.com>
2025-10-30 19:31:46 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let [a, b] = input.map(Number);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
