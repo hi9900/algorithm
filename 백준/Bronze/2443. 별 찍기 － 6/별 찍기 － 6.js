e9feb3360603db368c965eb53efd3ac6ce97a2ab
heeyoung <122499570+hi9900@users.noreply.github.com>
2025-11-05 20:00:52 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 84 ms, Memory: 9404 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+const N = Number(input[0]);
+let result = '';
+
+for (let i = 2 * N - 1; i > 0; i -= 2) {
+  result += `${' '.repeat((2 * N - 1 - i) / 2)}${'*'.repeat(i)}\n`;
+}
+
+console.log(result.trimEnd());
