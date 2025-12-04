10dda7e45611ae4ad5d0ca8767abaa8ee0b73036
heeyoung <122499570+hi9900@users.noreply.github.com>
2025-12-04 17:21:59 +0900

    [Bronze IV] Title: 세수정렬, Time: 96 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let arr = input[0].split(' ').map(Number);
+arr.sort((a, b) => a - b);
+
+console.log(arr.join(' '));
