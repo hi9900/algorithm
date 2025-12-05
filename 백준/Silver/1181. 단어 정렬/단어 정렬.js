553466f2243fa768ff65ba761c386e203fd595ab
heeyoung <122499570+hi9900@users.noreply.github.com>
2025-12-05 15:24:54 +0900

    [Silver V] Title: 단어 정렬, Time: 188 ms, Memory: 18096 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 function compare(a, b) {
+  if (a.length === b.length) {
+    if (a < b) return -1;
+    else if (a > b) return 1;
+    else return 0;
+  }
+
   return a.length - b.length;
 }
 
@@ -13,6 +19,6 @@ for (let i = 1; i <= n; i++) {
 }
 
 let arr = [...strSet];
-arr.sort();
+
 arr.sort(compare);
 console.log(arr.join('\n'));
