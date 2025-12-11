2e8b569f35c6d37a3bc41e172ad1c8859505f6b9
heeyoung <122499570+hi9900@users.noreply.github.com>
2025-12-11 15:15:49 +0900

    [Silver III] Title: 주유소, Time: 256 ms, Memory: 36400 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -5,11 +5,11 @@ let n = Number(input[0]);
 let road = input[1].split(' ').map(Number);
 let price = input[2].split(' ').map(Number);
 let minPrice = 1_000_000_001;
-let result = 0;
+let result = BigInt(0);
 
 for (let i = 0; i < n - 1; i++) {
   minPrice = Math.min(minPrice, price[i]);
-  result += minPrice * road[i];
+  result += BigInt(minPrice) * BigInt(road[i]);
 }
 
-console.log(result);
+console.log(String(result));
