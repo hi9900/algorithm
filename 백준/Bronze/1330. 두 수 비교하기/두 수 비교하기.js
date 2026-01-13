99f13fd818aeb4081d36100e56d2775f909158ba
heeyoung <122499570+hi9900@users.noreply.github.com>
2026-01-13 21:02:24 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 88 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let [a, b] = input[0].split(' ').map(Number);
+
+if (a > b) {
+  console.log('>');
+} else if (a < b) {
+  console.log('<');
+} else {
+  console.log('==');
+}
